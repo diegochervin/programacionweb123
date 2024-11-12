535b4d33fe519d26d744f05465c6356d2867dc25
diegochervin <diego.chervin@hotmail.com>
2024-11-12 15:36:22 -0300

    elit

--- nodePrueba/archivoback.js
+++
@@ -1,8 +0,0 @@
-console.log(`correr archivo de node js`)
-
-let mensaje = "HOLA PRUEBA DE LET"
-console.log(mensaje)
-
-//instalar NPM:  repositorio de modulos
-// todas las librerias que usamos o frameworks(sweetalert, toastify,luxon,bootstrap)
-
