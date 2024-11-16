f0fc8f079bc73f036f1e050c466a504ee21d847c
diego <diego.chervin@hotmail.com>
2024-11-16 19:48:32 -0300

    asdf

--- midu/fs-readfile.js
+++ midu/fs-readfile.js
@@ -1,20 +1,16 @@
-const fs = require(`node:fs`)
-console.log("leemos el primer archivo")
-const text = fs.readFile(`./archivo.txt`, `utf-8`, (err, text)=> {
-    console.log(text)
-})
-
-
+const fs = require('node:fs');
+console.log("leemos el primer archivo");
+const text = fs.readFile("./archivo.txt", "utf-8", (err, text) => {
+  console.log(text);
+});
 
 console.log("hacemos cosas de mientras")
 
-console.log(`leemos segundo archivo2`)
-const secondtext = fs.readFile(`./archivo2.txt`, `utf-8`,(err, secondtext) => {
-    console.log(secondtext)
-})
-
-
-
+console.log("leemos segundo archivo2")
+// eslint-disable-next-line n/handle-callback-err
+const secondtext = fs.readFile('./archivo2.txt', 'utf-8', (_err, secondtext) => {
+  console.log(secondtext)
+});
 
 // const text = fs.readFileSync(`./archivo.txt`, `utf-8`)
 // console.log(text)
